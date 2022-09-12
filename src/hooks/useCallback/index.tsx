2fb7620f5b4601823b6e920cb2141b675d11a03c
Neil <dev.haxuan@gmail.com>
2022-09-12 23:43:49 +0700

    refactor folder

--- src/hooks/useCallback/index.tsx
+++ src/hooks/useCallback/index.tsx
@@ -1,18 +1,1 @@
-import {useCallback, useState} from "react";
-import {Content} from "src/hooks/useCallback/content";
-
-export const DemoUseCallback = () => {
-    const [count, setCount] = useState<number>(0);
-
-    const handleChangeCount = useCallback(() => {
-        setCount((prevCount) => prevCount + 1);
-    }, []);
-
-    return (
-        <>
-            <Content onHandleIncrement={handleChangeCount}/>
-            <h2>Count: {count}</h2>
-            <button onClick={handleChangeCount}>Add 2</button>
-        </>
-    );
-};
+export * from "./useCallback";
